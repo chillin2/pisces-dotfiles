ba8c0b4e6256dba8290224ac68fff1b1d863cdd2
pisces <chillin2@naver.com>
2023-08-11 15:25:52 +0900

    folder tree change

---
+++ nvim/lua/pisces/base.lua
@@ -0,0 +1,58 @@
+vim.cmd("autocmd!")
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+vim.opt.fencs = { 'cp949', 'utf-8', 'ucs-bom' }
+
+vim.opt.shell = 'fish'
+
+vim.opt.cindent = true
+vim.opt.autoindent = true
+-- vim.opt.smartindent = true
+-- vim.opt.smarttab = true
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+
+vim.opt.wrap = false -- No Wrap lines
+-- [[ Setting options ]]
+-- See `:help vim.o`
+
+-- Set highlight on search
+vim.o.hlsearch = false
+
+-- Make line numbers default
+vim.wo.number = true
+
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.o.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+vim.o.background = "dark" -- or "light" for light mode
