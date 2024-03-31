73086f2670daaead3e424bec2763ea3e2a2b1812
pisces <chillin2@naver.com>
2024-03-31 13:26:32 +0900

    first commit

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,14 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fencs = { "utf-8", "cp949", "ucs-bom" }
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.shell = "fish"
+vim.opt.smarttab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
